63bf3cccf167c7249130f8a9a68826617015aaa5
Caio Eduardo Ribeiro Bento <caioersb@gmail.com>
2025-11-05 11:52:51 -0300

    Melhorar tratamento de erros da CLI

--- aplicação/main.go
+++ aplicação/main.go
@@ -11,7 +11,7 @@ func main() {
 	fmt.Println("Iniciando aplicação...")
 
 	aplicacao := app.Gerar()
-	if erro := aplicacao.Run(os.Args); erro != nil{
+	if erro := aplicacao.Run(os.Args); erro != nil {
 		log.Fatal(erro)
 	}
 }
